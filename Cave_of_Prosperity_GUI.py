edf5311f658d9b6c1dc4e35babfc84821334746b
jovin001 <john.vincent@zoho.com>
2015-07-19 22:05:24 -0400

    First commit for CoP GUI.

---
+++ Cave_of_Prosperity_GUI.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+from Tkinter import *
+
+#create the window
+root = Tk()
+
+#modify root windo
+root.title("Cave of Prosperity")
+root.geometry("200x100")
+
+#kick off the event loop.
+root.mainloop()
